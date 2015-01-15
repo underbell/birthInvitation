6b1485253825139f118f3134167e67cf6b810924
underbell <underbell.park@gmail.com>
2015-01-15 10:32:37 +0900

    kakao share

--- public/js/simple-app.js
+++ public/js/simple-app.js
@@ -47,5 +47,22 @@ $(function(){
 		}
 	})
 	swiperGallery.reInit()
+	
+	// 사용할 앱의 Javascript 키를 설정해 주세요.
+    Kakao.init('kr.pe.underbell');
 
+    // 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
+    Kakao.Link.createTalkLinkButton({
+      container: '#kakao-link-btn',
+      label: '소현이의 첫번째 생일을 축하해주세요~',
+      image: {
+        src: '/public/img/photo/invit01.png',
+        width: '480',
+        height: '800'
+      },
+      webButton: {
+        text: '소현이 돌잔치',
+        url: 'http://www.underbell.pe.kr' // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
+      }
+    });
 })
